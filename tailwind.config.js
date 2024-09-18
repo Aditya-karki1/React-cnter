6137f75f0ee863326dcd898adc98f2b7019ca623
Aditya-karki1 <adityakarki20247505@gmail.com>
2024-09-18 23:14:20 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // This ensures Tailwind scans all relevant files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
